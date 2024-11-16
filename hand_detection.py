d2fec994c2065a273714bcecaa3f459d6e1a6bd2
Annika Logarta <annikalogarta@gmail.com>
2024-11-16 14:20:01 -0500

    added global variables for steps

--- hand_detection.py
+++ hand_detection.py
@@ -5,6 +5,12 @@ import mediapipe as mp
 
 cap = cv.VideoCapture(0)
 
+global global_forward
+global global_backward
+global global_stop
+global global_turnLeft
+global global_turnRight
+
 if not cap.isOpened():
     print("Error: Could not open camera.")
     exit()
@@ -15,6 +21,13 @@ hands = mp_hands.Hands(static_image_mode=False, max_num_hands=1, min_detection_c
 mp_drawing = mp.solutions.drawing_utils
 
 while True:
+
+    global_forward = False
+    global_backward = False
+    global_stop = False
+    global_turnLeft = False
+    global_turnRight = False
+
     # Capture frame-by-frame
     ret, frame = cap.read()
     
@@ -28,15 +41,24 @@ while True:
         for hand_landmarks in results.multi_hand_landmarks:
             mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
     
-    index_finger_y = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y
-    thumb_y = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].y
+    index_finger = [hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x, hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y]
+    thumb = [hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].x, hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].y]
+    middle_finger = [hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].x, hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].y]
+    ring_finger = [hand_landmarks.landmark[mp_hands.HandLandmark.RING_FINGER_TIP].x , hand_landmarks.landmark[mp_hands.HandLandmark.RING_FINGER_TIP].y ]
 
-    if index_finger_y < thumb_y:
-        hand_gesture = 'pointing up'
-    elif index_finger_y > thumb_y:
-        hand_gesture = 'pointing down'
+    pointingBool = any(
+    (index_finger[i] < thumb[i] < middle_finger[i]) or
+    (index_finger[i] > thumb[i] > middle_finger[i])
+    for i in range(2))
+
+    if pointingBool and (index_finger[1] < thumb[1]):
+        global_forward = True
+        hand_gesture = 'Pointing up - going forward'
+    elif pointingBool and (index_finger[1] > thumb[1]):
+        global_backward = True
+        hand_gesture = 'pointing down - going backward'
     else:
-        hand_gesture = 'other'
+        hand_gesture = 'No gesture detected'
 
     
     cv.putText(frame, hand_gesture, (10,60), cv.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 1 )
