14f69d18c9fe70fa1e3a3ee0f5e11ec2d3a56cf3
Kimberley Hoang <kimberley.hoang528@gmail.com>
2024-11-16 16:46:14 -0500

    Add sleep

--- controllers/epuck_controller_hd_v3/epuck_controller_hd_v3.py
+++ controllers/epuck_controller_hd_v3/epuck_controller_hd_v3.py
@@ -4,6 +4,7 @@ import numpy as np
 import pyautogui as pya
 import mediapipe as mp
 import threading
+import time
 
 # Webots Robot Setup
 robot = Robot()
@@ -124,6 +125,7 @@ def process_gesture():
             global_backward = False
             global_turnRight = False
             global_turnLeft = False
+            time.sleep(2.5)
             break
 
 # Function for controlling robot's motion based on gestures
