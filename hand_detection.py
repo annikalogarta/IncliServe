16a835a7c7510c90546b8d657f80270ab7cccca4
Kimberley Hoang <kimberley.hoang528@gmail.com>
2024-11-16 14:20:46 -0500

    Fix hand landmarks error

--- hand_detection.py
+++ hand_detection.py
@@ -24,28 +24,30 @@ while True:
 
     image_rgb = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
     results = hands.process(image_rgb)
+
     if results.multi_hand_landmarks:
         for hand_landmarks in results.multi_hand_landmarks:
             mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
     
-    index_finger_y = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y
-    thumb_y = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].y
+        index_finger_y = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y
+        thumb_y = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].y
 
-    if index_finger_y < thumb_y:
-        hand_gesture = 'pointing up'
-    elif index_finger_y > thumb_y:
-        hand_gesture = 'pointing down'
-    else:
-        hand_gesture = 'other'
+        if index_finger_y < thumb_y:
+            hand_gesture = 'pointing up'
+        elif index_finger_y > thumb_y:
+            hand_gesture = 'pointing down'
+        else:
+            hand_gesture = 'other'
 
-    
-    cv.putText(frame, hand_gesture, (10,60), cv.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 1 )
-    
+        
+        cv.putText(frame, hand_gesture, (10,60), cv.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 1 )
+        
     # Display the resulting frame
     cv.imshow('Live Video', frame)
 
     # Break the loop when 'q' is pressed
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
+
 cap.release()
 cv.destroyAllWindows()
